7fdad31e07b2bcf0297d8318b0d1bfac90b7dc0d
Francisco Gutiérrez López <93456432+FrancoGL20@users.noreply.github.com>
2025-04-28 23:18:31 -0600

    feat: Refactor MappingPage to add form field and improve UI layout

--- src/ui/xml_templates/mapping.py
+++ src/ui/xml_templates/mapping.py
@@ -1,25 +1,50 @@
-import os
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QGroupBox, QVBoxLayout, QFormLayout, QLineEdit
 from PySide6.QtCore import Qt
 
 class MappingPage(QWidget):
-    """Mapping xml templates function."""
+    """Mapping xml templates function view."""
     
     def __init__(self):
         super().__init__()
         self._setup_ui()
-        self.preferred_width = 400
-        self.preferred_height = 500
+        self.PREFERRED_WIDTH = 600
+        self.PREFERRED_HEIGHT = 450
 
+    
     def _setup_ui(self):
-        """Configure the user interface of the mapping page."""
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+        """Configure the user interface."""
+        self.main_layout = QVBoxLayout()
+        self.setLayout(self.main_layout)
         
-        # Add specific widgets for your main page here
+        group_box = self._create_group_box()
+        self.main_layout.addWidget(group_box)
+    
+    
+    def _create_group_box(self):
+        """Create and configure the main group box."""
+        group_box = QGroupBox()
+        layout = QVBoxLayout()
+        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        
+        # Title
         welcome_label = QLabel("Mapping xml templates function.")
+        welcome_label.setStyleSheet("font-size: 15px; font-weight: bold; margin-left: 20px; margin-bottom: 20px;")
         welcome_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(welcome_label)
+        
+        # Files and directory
+        form_layout_widget = QWidget()
+        form_layout = QFormLayout()
+        form_layout.setVerticalSpacing(15)
+        form_layout.addRow(QLabel("Input directory:"), QLineEdit())
+        form_layout.addRow(QLabel("Output directory:"), QLineEdit())
+        form_layout.addRow(QLabel("Mapping file:"), QLineEdit())
+        form_layout_widget.setLayout(form_layout)
+        layout.addWidget(form_layout_widget)
+        
+        group_box.setLayout(layout)
+        return group_box
+    
     
     def get_preferred_size(self) -> tuple[int, int]:
         """Returns the preferred size for this page.
@@ -27,4 +52,4 @@ class MappingPage(QWidget):
         Returns:
             tuple: (width, height) in pixels
         """
-        return self.preferred_width, self.preferred_height
+        return self.PREFERRED_WIDTH, self.PREFERRED_HEIGHT
