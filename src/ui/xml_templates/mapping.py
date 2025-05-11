d8d52ad6be2e337942749d4f980f26d64bf0f8ab
Francisco Gutiérrez López <93456432+FrancoGL20@users.noreply.github.com>
2025-05-11 14:36:00 -0600

    feat: Add custom column mapping functionality and update UI elements in MappingPage

--- src/ui/xml_templates/mapping.py
+++ src/ui/xml_templates/mapping.py
@@ -10,12 +10,12 @@ class MappingPage(QWidget):
     def __init__(self, main_window=None):
         self.PREFERRED_WIDTH = 1000
         self.PREFERRED_HEIGHT = 450
-        self.JSON_DIR_INPUT = 'directory_input'
         self.JSON_DIR_OUTPUT = 'directory_output'
         self.JSON_FILE_MAPPING = 'file_mapping'
         self.JSON_FILE_TO_MAP = 'file_to_map'
         self.JSON_CHECKBOX_DETAILS = 'checkbox_details'
         self.JSON_CHECKBOX_UNMAP = 'checkbox_unmap'
+        self.JSON_CHECKBOX_CUSTOM_COLUMN_TO_MAP = 'checkbox_custom_column_to_map'
         
         self.main_window = main_window
         self.controller = MappingController()
@@ -51,12 +51,12 @@ class MappingPage(QWidget):
             data = memory_content
 
             # Update UI elements with data from the memory file
-            self.input_directory_line_edit.setText(data.get(self.JSON_DIR_INPUT, ''))
             self.output_directory_line_edit.setText(data.get(self.JSON_DIR_OUTPUT, ''))
             self.mapping_file_line_edit.setText(data.get(self.JSON_FILE_MAPPING, ''))
             self.file_to_map_line_edit.setText(data.get(self.JSON_FILE_TO_MAP, ''))
             self.checkbox_details.setChecked(data.get(self.JSON_CHECKBOX_DETAILS, False))
             self.checkbox_unmap.setChecked(data.get(self.JSON_CHECKBOX_UNMAP, False))
+            self.checkbox_custom_column_to_map.setChecked(data.get(self.JSON_CHECKBOX_CUSTOM_COLUMN_TO_MAP, False))
     
     def _create_group_box(self):
         """Create and configure the main group box."""
@@ -77,59 +77,63 @@ class MappingPage(QWidget):
         formulary_grid_layout = QGridLayout()
         formulary_grid_layout.setVerticalSpacing(15)
         
-        # Input directory
-        formulary_grid_layout.addWidget(QLabel("Input directory:"), 0, 0)
-        self.input_directory_line_edit = QLineEdit()
-        formulary_grid_layout.addWidget(self.input_directory_line_edit, 0, 1)
-        button_browse_input_dir = QPushButton("Browse")
-        button_browse_input_dir.clicked.connect(lambda: self._on_browse(0))
-        button_browse_input_dir.setToolTip("Click to select the input directory.")
-        formulary_grid_layout.addWidget(button_browse_input_dir, 0, 2)
+        # File to map
+        formulary_grid_layout.addWidget(QLabel("File to map:"), 1, 0)
+        self.file_to_map_line_edit = QLineEdit()
+        formulary_grid_layout.addWidget(self.file_to_map_line_edit, 1, 1)
+        button_browse_file_to_map = QPushButton("Browse")
+        button_browse_file_to_map.clicked.connect(lambda: self._on_browse(1))
+        button_browse_file_to_map.setToolTip("Click to select the file to map.")
+        formulary_grid_layout.addWidget(button_browse_file_to_map, 1, 2)
         
         # Output directory
-        formulary_grid_layout.addWidget(QLabel("Output directory:"), 1, 0)
+        formulary_grid_layout.addWidget(QLabel("Output directory:"), 2, 0)
         self.output_directory_line_edit = QLineEdit()
-        formulary_grid_layout.addWidget(self.output_directory_line_edit, 1, 1)
+        formulary_grid_layout.addWidget(self.output_directory_line_edit, 2, 1)
         button_browse_output_dir = QPushButton("Browse")
-        button_browse_output_dir.clicked.connect(lambda: self._on_browse(1))
+        button_browse_output_dir.clicked.connect(lambda: self._on_browse(2))
         button_browse_output_dir.setToolTip("Click to select the output directory.")
-        formulary_grid_layout.addWidget(button_browse_output_dir, 1, 2)
+        formulary_grid_layout.addWidget(button_browse_output_dir, 2, 2)
         
         # Mapping file
-        formulary_grid_layout.addWidget(QLabel("Mapping file:"), 2, 0)
+        formulary_grid_layout.addWidget(QLabel("Mapping file:"), 3, 0)
         self.mapping_file_line_edit = QLineEdit()
-        formulary_grid_layout.addWidget(self.mapping_file_line_edit, 2, 1)
+        formulary_grid_layout.addWidget(self.mapping_file_line_edit, 3, 1)
         button_browse_mapping_file = QPushButton("Browse")
-        button_browse_mapping_file.clicked.connect(lambda: self._on_browse(2))
+        button_browse_mapping_file.clicked.connect(lambda: self._on_browse(3))
         button_browse_mapping_file.setToolTip("Click to select the mapping file.")
-        formulary_grid_layout.addWidget(button_browse_mapping_file, 2, 2)
+        formulary_grid_layout.addWidget(button_browse_mapping_file, 3, 2)
         
-        # File to map
-        formulary_grid_layout.addWidget(QLabel("File to map:"), 3, 0)
-        self.file_to_map_line_edit = QLineEdit()
-        formulary_grid_layout.addWidget(self.file_to_map_line_edit, 3, 1)
-        button_browse_file_to_map = QPushButton("Browse")
-        button_browse_file_to_map.clicked.connect(lambda: self._on_browse(3))
-        button_browse_file_to_map.setToolTip("Click to select the file to map.")
-        formulary_grid_layout.addWidget(button_browse_file_to_map, 3, 2)
-
         # Formulary widget - End
         formulary_widget.setLayout(formulary_grid_layout)
         page_layout.addWidget(formulary_widget)
 
-        # Checkbox for details and unmap
+        # Group of checkboxes
         checkbox_group=QWidget()
         checkbox_layout=QHBoxLayout()
         checkbox_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
         checkbox_layout.setSpacing(50)
         checkbox_group.setLayout(checkbox_layout)
-        
+        # Checkbox for details
         self.checkbox_details = QCheckBox("Has details")
         self.checkbox_details.setToolTip("Check if the file to map has details.")
         checkbox_layout.addWidget(self.checkbox_details)
+        # Checkbox for unmap
         self.checkbox_unmap = QCheckBox("Unmap")
         self.checkbox_unmap.setToolTip("Check if you want to unmap the file.")
         checkbox_layout.addWidget(self.checkbox_unmap)
+        # Checkbox for custom column to map
+        self.checkbox_custom_column_to_map = QCheckBox("Custom column to map")
+        self.checkbox_custom_column_to_map.setToolTip("Check if you want to map a custom column (other than \"TM Tariff ID\",\"Tariff ID\",\"TariffCode\").")
+        self.checkbox_custom_column_to_map.checkStateChanged.connect(self._on_custom_column_to_map)
+        checkbox_layout.addWidget(self.checkbox_custom_column_to_map)
+        # Line edit for custom column to map
+        self.line_edit_custom_column_to_map = QLineEdit()
+        self.line_edit_custom_column_to_map.setPlaceholderText("Custom column to map")
+        self.line_edit_custom_column_to_map.setToolTip("Disabled")
+        self.line_edit_custom_column_to_map.setEnabled(False)
+        checkbox_layout.addWidget(self.line_edit_custom_column_to_map)
+        # Group of checkboxes - End
         page_layout.addWidget(checkbox_group)
         
         # Buttons
@@ -143,30 +147,25 @@ class MappingPage(QWidget):
     
     
     def _on_browse(self, line_edit_index: int):
-        """Handle the browse button click for the input, output, and mapping file."""
+        """Handle the browse button click for the output and mapping file."""
         
-        if line_edit_index == 0:
-            # Browse for input directory
-            directory = Path(QFileDialog.getExistingDirectory(self, "Select Input Directory"))
-            if directory:
-                self.input_directory_line_edit.setText(directory.as_posix())
-        elif line_edit_index == 1:
+        if line_edit_index == 1:
+            # Browse for file to map
+            file_name, _ = QFileDialog.getOpenFileName(self, "Select File to Map", "", "XML Files (*.xml)")
+            file_name = Path(file_name)
+            if file_name:
+                self.file_to_map_line_edit.setText(file_name.as_posix())
+        elif line_edit_index == 2:
             # Browse for output directory
             directory = Path(QFileDialog.getExistingDirectory(self, "Select Output Directory"))
             if directory:
                 self.output_directory_line_edit.setText(directory.as_posix())
-        elif line_edit_index == 2:
+        elif line_edit_index == 3:
             # Browse for mapping file
             file_name, _ = QFileDialog.getOpenFileName(self, "Select Mapping File", "", "Excel Files (*.xlsx)")
+            file_name = Path(file_name)
             if file_name:
-                self.mapping_file_line_edit.setText(file_name)
-        elif line_edit_index == 3:
-            # Browse for file to map
-            file_name, _ = QFileDialog.getOpenFileName(self, "Select File to Map", "", "XML Files (*.xml)")
-            # Extract the file name from the path
-            file_name = Path(file_name).name
-            if file_name:
-                self.file_to_map_line_edit.setText(file_name)
+                self.mapping_file_line_edit.setText(file_name.as_posix())
     
     
     def _on_map_file(self):
@@ -174,19 +173,24 @@ class MappingPage(QWidget):
         
         # Save memory content
         JSON_DATA = {
-            self.JSON_DIR_INPUT: self.input_directory_line_edit.text(),
             self.JSON_DIR_OUTPUT: self.output_directory_line_edit.text(),
             self.JSON_FILE_MAPPING: self.mapping_file_line_edit.text(),
             self.JSON_FILE_TO_MAP: self.file_to_map_line_edit.text(),
             self.JSON_CHECKBOX_DETAILS: self.checkbox_details.isChecked(),
-            self.JSON_CHECKBOX_UNMAP: self.checkbox_unmap.isChecked()
+            self.JSON_CHECKBOX_UNMAP: self.checkbox_unmap.isChecked(),
+            self.JSON_CHECKBOX_CUSTOM_COLUMN_TO_MAP: self.checkbox_custom_column_to_map.isChecked()
         }
         self.controller.save_memory_content(JSON_DATA)
         
         # TODO: Implement the mapping logic here.
         print("Mapping file...")
     
-    
+    def _on_custom_column_to_map(self):
+        """Handle custom column to map checkbox state change."""
+        
+        self.line_edit_custom_column_to_map.setEnabled(self.checkbox_custom_column_to_map.isChecked())
+        self.line_edit_custom_column_to_map.setToolTip("Disabled" if not self.checkbox_custom_column_to_map.isChecked() else "Specify the column name to map")
+
     def get_preferred_size(self) -> tuple[int, int]:
         """Returns the preferred size for this page.
         
---
+++ src/utils/xml_files_processing.py
@@ -0,0 +1,129 @@
+from pathlib import Path
+import xml.etree.ElementTree as ET
+import pandas as pd
+import shutil
+import os
+
+# TODO: Finish the function
+
+def map_ids(
+        FILE_WITH_MAPPING:str,
+        FILE_TO_MAP:str,
+        FILE_MAPPED:str,
+        COLUMNS_TO_MAP:list,
+        UNMAP:bool=False,
+        HAS_DETAILS:bool=False,
+    ) -> str|set:
+    
+    columns_mapping={
+        "tariff_id_to_map":"Tariff ID", # Original ID to map
+        "tariff_number":"Tariff Number", # New ID
+    }
+
+    if UNMAP:
+        columns_mapping={
+            "tariff_id_to_map":"Tariff Number", # New ID
+            "tariff_number":"Tariff ID", # Original ID to map
+        }
+
+    # Verify if the output file exists and remove it
+    if os.path.exists(FILE_MAPPED):
+        os.remove(FILE_MAPPED)
+
+    # Copy the file to map to the output file (to modify it)
+    shutil.copy(FILE_TO_MAP,FILE_MAPPED)
+
+    # Read mapping file
+    columns_mapping={value:key for key,value in columns_mapping.items()}
+    try:
+        df_mapping=pd.read_excel(FILE_WITH_MAPPING,dtype="string").loc[:,list(columns_mapping.keys())].rename(columns=columns_mapping)
+    except Exception as e:
+        return str(e)
+
+    # Convert mapping file to dictionary
+    DICT_MAPPING=df_mapping.set_index("tariff_id_to_map")["tariff_number"].to_dict()
+
+    # * Modify the result file
+
+    # Parse the file
+    tree=ET.parse(FILE_MAPPED)
+    root=tree.getroot()
+    namespace={
+        "ss":"urn:schemas-microsoft-com:office:spreadsheet"
+    }
+    data=root.find("./ss:Worksheet[@ss:Name='Data']",namespace)
+    table=data.find("./ss:Table",namespace)
+
+    # Counters
+    row_number=1
+    errors_number=0
+    mapped_counter=0
+    index_of_mapping_column=None
+
+    # Iterate over rows
+    for row in table.iterfind("./ss:Row",namespace):
+        column_number=1
+        for cell in row.iterfind("./ss:Cell",namespace):
+            # If there is a index value
+            #     # need to modify the index to specified
+            if cell.get(f"{{{namespace['ss']}}}Index") is not None:
+                index_text_value=cell.get(f"{{{namespace['ss']}}}Index")
+                column_number = int(index_text_value) if index_text_value else column_number
+
+            # If the row text is in the columns to map
+            #     # Save the index of the column to map for the next rows
+            if row_number==1:
+                if cell.find("./ss:Data",namespace).text in COLUMNS_TO_MAP:
+                    index_of_mapping_column=column_number
+                    break # go to next row
+                column_number+=1
+                continue # go to next cell (column)
+            
+            # If there is no column to map in the file
+            if row_number==2 and index_of_mapping_column is None:
+                os.remove(FILE_MAPPED)
+                return "No column to map was found"
+            
+            # If the row is a detail row or the file has details
+            #     # Skip it and not count it in the row total
+            if (row_number==2 and HAS_DETAILS) or (column_number==1 and cell.find("./ss:Data",namespace).text=="D"):
+                break # go to next row
+            
+            # If the column to map is found 
+            # and the cell has the same column number as the index of the column to map
+            if (index_of_mapping_column is not None) and (column_number==index_of_mapping_column):
+                original_id=cell.find("./ss:Data",namespace)
+                # If the original id has a mapping value
+                if DICT_MAPPING.get(original_id.text) is not None:
+                    new_id=DICT_MAPPING[original_id.text]
+                # If the original id has no mapping value
+                else:
+                    new_id="XXXXX"
+                    errors_number+=1
+                
+                original_id.text=new_id
+                mapped_counter+=1
+                break # go to next row (avoiding the rest of the cells in the row)
+            
+            column_number+=1
+
+        row_number+=1
+
+    # Write the file with the changes
+    tree.write(FILE_MAPPED,encoding="UTF-8", xml_declaration=True,short_empty_elements=False)
+
+    # Row total, mapped and errors
+    return (row_number-1,mapped_counter,errors_number)
+
+
+INPUT_DIRECTORY=Path(r"C:\Users\francisco.gutierrez\OneDrive - Netlogistik\Documentos\Python scripts (useful for any project)\Mapeo de IDs\Input")
+OUPUT_DIRECTORY=Path(r"C:\Users\francisco.gutierrez\OneDrive - Netlogistik\Documentos\Python scripts (useful for any project)\Mapeo de IDs\Output")
+FILE_WITH_MAPPING=Path(r"C:\Users\francisco.gutierrez\OneDrive - Netlogistik\Documentos\Python scripts (useful for any project)\Mapeo de IDs\Input\Mapping AP V3 con _.xlsx")
+TEMP_FILE_TO_MAP="AP Tariff 02 - Services"
+
+COLUMNS_TO_MAP=["TM Tariff ID","Tariff ID","TariffCode"]
+TEMP_FILE_TO_MAP_EXT="xml"
+FILE_TO_MAP=f"{INPUT_DIRECTORY}/{TEMP_FILE_TO_MAP}.{TEMP_FILE_TO_MAP_EXT}"
+FILE_MAPPED=f"{OUPUT_DIRECTORY}/{TEMP_FILE_TO_MAP} - Mapped.{TEMP_FILE_TO_MAP_EXT}"
+HAS_DETAILS=False
+UNMAP=False
